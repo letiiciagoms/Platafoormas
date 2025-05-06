e61a20aa6af0bfdd8e2406e3da46b5924805d812
letiiciagoms <145374951+letiiciagoms@users.noreply.github.com>
2025-05-06 16:15:02 -0300

    atv

--- Atividade 01 - Plataformas/Assets/Scripts/Player.cs
+++ Atividade 01 - Plataformas/Assets/Scripts/Player.cs
@@ -1,16 +1,34 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5f;
+
+    private Rigidbody2D rig;
+    
     public int moedas = 0;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
+        Move();
         if (Keyboard.current.mKey.wasPressedThisFrame)
         {
             moedas++;
             PlayerObserverManager.ChangedMoedas(moedas);
         }
+        
+    }
+
+    void Move()
+    {
+        rig.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, rig.linearVelocity.y);
         
     }
 }
